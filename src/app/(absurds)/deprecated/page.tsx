b14e933dbcc482479527190784d608191f26b47f
agung saepudin <saepudinjeh@gmail.com>
2025-02-25 21:42:54 +0700

    added absurds deprecated

---
+++ src/app/(absurds)/deprecated/page.tsx
@@ -0,0 +1,29 @@
+import LayoutQuote from "@/components/commons/layout_quote.common";
+import NavbarCustom from "@/components/commons/navbar.common";
+
+export default function Deprecated() {
+    return (
+        <NavbarCustom>
+            <LayoutQuote>
+                <h1 className="sm:text-lg font-semibold">Deprecated</h1>
+                <p className="text-xs text-gray-500">25/02/2025 21:10</p>
+
+                <div className="mt-7 flex flex-col gap-2">
+                    <p>Sejujurnya sudah lama sekali aku tidak menulis. Di History Git-ku tercatat terakhir kali tersentuh 17 bulan yang lalu. Semua package library di framework ini mungkin sudah banyak yang deprecated ketika diupgrade. Nyatanya sudah aku coba, semuanya error dan semuanya berantakan. Rasanya ingin buat dari awal lagi. Itu tidak akan terjadi. Biarlah seperti itu. Malas</p>
+
+                    <p>Dari sini aku mulai berfikir apakah semua mimpi, harapan, kenginan apakah akan deprecated ketika seiring berjalannya waktu. Apakah grow up termasuk upgrade diri? apakah semua mimpi dan harapan tidak akan deprecated? apakah akan pindah haluan? Membuat mimpi baru? Harapan baru? apakah semuanya akan terulang-ulang mengikuti keputusan yang telah kita ambil. Lifecycle yang buruk. Aku tau itu.</p>
+
+                    <p>Aku selalu berpegang teguh dengan <b>Aku yang sekarang harus nyenengin aku yang akan datang, dan aku yang akan datang harus nyenengin aku yang sekarang</b>, intinya apa yang akan aku lakukan dan keputusan apa yang aku ambil sekarang harus menentukan aku yang akan datang dan apa yang aku lakukan di masa mendatang harus menyenangkan aku yang sekarang. Dan kau tau aku baru saja membeli <i>Nintendo Switch</i>, sebuah <i>console handheld</i>.</p>
+
+                    <p>Kau tau apa maksudku? Aku ingat sekali ketika aku cuma diem melihat teman membawa <i>gimbot</i> atau <i>gameboy</i> kalau kalian tau itu handheld terbaik diwaktu itu dan hanya orang yang punya duit saja yang bisa punya itu. Kau tau aku ada dimana? Yaps senyum-senyum sendiri ketika mereka memainkan itu. Aku tau kalau aku mencoba minjem mereka pasti nolak dengan alasan baterai-nya mau habis, aku tau kalau aku hanya menyentuhnya mereka akan beralasan mau pindah posisi duduk, aku juga tau rasanya sangat menyenangkan hanya cuma melihat mereka bermain</p>
+
+                    <p>Dan sekarang aku sudah membeli <i>Nintendo Switch</i>. Untuk siapa? Yaps, untuk diriku sendiri dimasa lalu. Ini agak dramatis, kalau aku bisa ke masa lalu jelas aku akan kasikan itu padanya. Ingin sekali melihat dia tersenyum, sambil memegang memainkan <i>switch</i>. Bila perlu kau sombongkan ke mereka, untuk seumuranmu tentu itu membuatmu senang bisa menyombongkan hal itu.</p>
+
+                    <p>Dan aku tau apa yang aku lakuin sekarang karena usahaku yang dulu, sebuah pengalaman dan kerja keras tentu itu bukan jalan pintas.</p>
+
+                    <p><i>Terima kasih untuk diriku sendiri sudah berjalan terlalu jauh</i>.</p>
+                </div>
+            </LayoutQuote>
+        </NavbarCustom>
+    )
+}
